f288b8b12f6f960919e8faf63cebea05f9a75098
Dmitry <119443995+dmitry-stepson@users.noreply.github.com>
2022-12-03 12:52:35 +0300

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Dmitry B. on 03.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
